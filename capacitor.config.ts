d3c7e3ff227c0284af654a20a0d2b30cce240538
John Gualteros <gualterosjohn40@gmail.com>
2022-09-03 13:08:03 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'login_design_ionic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
